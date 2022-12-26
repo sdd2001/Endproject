1a0ca960a65af7350b331f3569eee8d2bda48a8a
Sean De Decker <74727305+sdd2001@users.noreply.github.com>
2022-12-26 22:52:53 +0100

    Get Games method
    Get games method added to DBHandler
    Get games method implemented on MainActivity.kt

--- app/src/main/java/com/example/endproject/MainActivity.kt
+++ app/src/main/java/com/example/endproject/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.endproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.endproject.classes.DatabaseHandler
+import com.example.endproject.classes.Game
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    private fun getGamesList(): ArrayList<Game> {
+        val databaseHandler: DatabaseHandler = DatabaseHandler(this)
+        val gamesList: ArrayList<Game> = databaseHandler.viewGames()
+
+        return gamesList
 
     }
 
--- app/src/main/java/com/example/endproject/classes/DatabaseHandler.kt
+++ app/src/main/java/com/example/endproject/classes/DatabaseHandler.kt
@@ -1,7 +1,10 @@
 package com.example.endproject.classes
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteException
 import android.database.sqlite.SQLiteOpenHelper
 
 class DatabaseHandler(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
@@ -35,6 +38,48 @@ class DatabaseHandler(context: Context) : SQLiteOpenHelper(context, DATABASE_NAM
     override fun onUpgrade(db: SQLiteDatabase?, old: Int, new: Int) {
         db!!.execSQL("DROP TABLE IF EXISTS $TABLE_CONTACTS")
         onCreate(db);
+
+    }
+
+    @SuppressLint("Range")
+    fun viewGames(): ArrayList<Game> {
+        val gamesList: ArrayList<Game> = ArrayList<Game>()
+
+        val selectQuery = "SELECT * FROM $TABLE_CONTACTS"
+
+        val db = this.readableDatabase
+        var cursor: Cursor? = null
+
+        try  {
+            cursor = db.rawQuery(selectQuery, null)
+
+        } catch (e: SQLiteException) {
+            db.execSQL(selectQuery)
+            return ArrayList()
+
+        }
+
+        var id: Int
+        var name: String
+        var descr: String
+        var genre: String
+        var developer: String
+        var year: Int
+
+        if (cursor.moveToFirst()) {
+            do {
+                id = cursor.getInt(cursor.getColumnIndex(KEY_ID))
+                name = cursor.getString(cursor.getColumnIndex(KEY_NAME))
+                descr = cursor.getString(cursor.getColumnIndex(KEY_DESCRIPTION))
+                genre = cursor.getString(cursor.getColumnIndex(KEY_GENRE))
+                developer = cursor.getString(cursor.getColumnIndex(KEY_DEVELOPER))
+                year = cursor.getInt(cursor.getColumnIndex(KEY_YEAR))
+
+            } while (cursor.moveToNext())
+
+        }
+
+        return gamesList
 
     }
 
